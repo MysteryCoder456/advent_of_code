2ef1402745fde7bdef04f6b8e9081a4d9b0534fe
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-04-28 12:08:12 +0400

    2023 C Day 4 Part 2

--- 2023/c/day4/day4.c
+++ 2023/c/day4/day4.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LINE_BUFFER_SIZE 128
+#define HEIGHT 193
+#define WINNING_CARD_COUNT 10
+#define MY_CARD_COUNT 25
 
 int part1() {
     FILE *fptr = fopen("day4.txt", "r");
@@ -10,20 +14,25 @@ int part1() {
     int totalPoints = 0;
 
     while (fgets(line, LINE_BUFFER_SIZE, fptr)) {
-        int winningCards[10];
-        int myCards[25];
+        int winningCards[WINNING_CARD_COUNT];
+        int myCards[MY_CARD_COUNT];
 
-        for (int i = 0; i < 10; i++) {
-            winningCards[i] = atoi(&line[3 * i + 10]);
+        char *colon = strstr(line, ":");
+        char *pipe = strstr(line, "|");
+
+        // Parsing both card piles
+        for (int i = 0; i < WINNING_CARD_COUNT; i++) {
+            winningCards[i] = atoi(&colon[3 * i + 2]);
         }
-        for (int i = 0; i < 25; i++) {
-            myCards[i] = atoi(&line[3 * i + 42]);
+        for (int i = 0; i < MY_CARD_COUNT; i++) {
+            myCards[i] = atoi(&pipe[3 * i + 2]);
         }
 
         int points = 0;
 
-        for (int i = 0; i < 25; i++) {
-            for (int j = 0; j < 10; j++) {
+        // Checking for winning cards
+        for (int i = 0; i < MY_CARD_COUNT; i++) {
+            for (int j = 0; j < WINNING_CARD_COUNT; j++) {
                 if (myCards[i] == winningCards[j]) {
                     points = points > 0 ? points * 2 : 1;
                     break;
@@ -38,9 +47,64 @@ int part1() {
     return totalPoints;
 }
 
+int getTotalCardsFromLine(int cardNum, char grid[HEIGHT][LINE_BUFFER_SIZE],
+                          int memoized[HEIGHT]) {
+    if (memoized[cardNum - 1] != 0)
+        return memoized[cardNum - 1];
+
+    // printf("Processing card %d\n", cardNum);
+    char *line = grid[cardNum - 1];
+
+    int winningCards[WINNING_CARD_COUNT];
+    int myCards[MY_CARD_COUNT];
+
+    char *colon = strstr(line, ":");
+    char *pipe = strstr(line, "|");
+
+    // Parsing both card piles
+    for (int i = 0; i < WINNING_CARD_COUNT; i++) {
+        winningCards[i] = atoi(&colon[3 * i + 2]);
+    }
+    for (int i = 0; i < MY_CARD_COUNT; i++) {
+        myCards[i] = atoi(&pipe[3 * i + 2]);
+    }
+
+    // Checking for winning cards
+    int winningCardCount = 0;
+    for (int i = 0; i < MY_CARD_COUNT; i++) {
+        for (int j = 0; j < WINNING_CARD_COUNT; j++) {
+            if (myCards[i] == winningCards[j]) {
+                winningCardCount++;
+            }
+        }
+    }
+
+    // Get card counts from card copies that we won from the current card
+    int totalCards = 1; // Includes the current card
+    for (int i = 1; i <= winningCardCount; i++) {
+        totalCards += getTotalCardsFromLine(cardNum + i, grid, memoized);
+    }
+    memoized[cardNum - 1] = totalCards;
+    return totalCards;
+}
+
 int part2() {
-    // TODO
-    return 0;
+    FILE *fptr = fopen("day4.txt", "r");
+    char grid[HEIGHT][LINE_BUFFER_SIZE];
+
+    // Read file into grid
+    for (int i = 0; i < HEIGHT; i++)
+        fgets(grid[i], LINE_BUFFER_SIZE, fptr);
+
+    fclose(fptr);
+
+    int totalCards = 0;
+    int memoized[HEIGHT] = {0};
+
+    for (int cardNum = 1; cardNum <= HEIGHT; cardNum++)
+        totalCards += getTotalCardsFromLine(cardNum, grid, memoized);
+
+    return totalCards;
 }
 
 int main() {
