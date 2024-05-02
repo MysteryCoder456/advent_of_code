67aa98f65f99bbbc2cbdb0c69d78b6a595898a2d
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-05-02 21:34:30 +0400

    2023 C day 5 part 2 (UNFINISHED)

--- 2023/c/day5/day5.c
+++ 2023/c/day5/day5.c
@@ -92,8 +92,88 @@ uint64_t part1() {
 }
 
 uint64_t part2() {
-    // TODO
-    return 0;
+    FILE *fptr = fopen("day5.txt", "r");
+    char lineBuffer[MAX_LINE];
+
+    // Skip to first map section
+    fgets(lineBuffer, MAX_LINE, fptr);
+    fgets(lineBuffer, MAX_LINE, fptr);
+
+    uint64_t ranges[7][64][3];
+    int rangeCounts[7];
+    int section = 0;
+
+    // Parse map sections
+    while (!feof(fptr)) {
+        // Skip map heading
+        fgets(lineBuffer, MAX_LINE, fptr);
+
+        int rangeCount = 0;
+
+        // Parse ranges and store them
+        fgets(lineBuffer, MAX_LINE, fptr);
+        while (*lineBuffer != '\n' && !feof(fptr)) {
+            char *rangeStr = strdup(lineBuffer);
+
+            // Store each component of range
+            for (int k = 0; k < 3; k++)
+                ranges[section][rangeCount][k] = atol(strsep(&rangeStr, " "));
+
+            free(rangeStr);
+            fgets(lineBuffer, MAX_LINE, fptr);
+            rangeCount++;
+        }
+
+        rangeCounts[section] = rangeCount;
+        section++;
+    }
+
+    // Seek to beginning of file and get seed ranges list
+    fseek(fptr, 0, 0);
+    fgets(lineBuffer, MAX_LINE, fptr);
+    fclose(fptr);
+    char *seedsBuffer = &lineBuffer[7];
+
+    // Parse seed ranges and find minimum location value
+    // FIXME: very very slow, pls multithread
+    uint64_t minLocation = UINT64_MAX;
+    char *first;
+    while ((first = strsep(&seedsBuffer, " ")) != NULL) {
+        char *second = strsep(&seedsBuffer, " ");
+
+        uint64_t start = atol(first);
+        uint64_t size = atol(second);
+        printf("processing seed range %lld : %lld\n", start, size);
+
+        // Seeds
+        for (uint64_t seed = start; seed < start + size; seed++) {
+            uint64_t mappedSeed = seed;
+
+            if (seed % 10000000 == 0)
+                printf("processing seed %lld\n", seed);
+
+            // Sections
+            for (int i = 0; i < 7; i++) {
+                int rangeCount = rangeCounts[i];
+
+                // Ranges
+                for (int j = 0; j < rangeCount; j++) {
+                    uint64_t *range = ranges[i][j];
+                    uint64_t seedBefore = mappedSeed;
+                    mappedSeed =
+                        mapRange(mappedSeed, range[0], range[1], range[2]);
+
+                    if (mappedSeed != seedBefore)
+                        break;
+                }
+            }
+
+            if (mappedSeed < minLocation)
+                minLocation = mappedSeed;
+        }
+    }
+
+    return minLocation;
 }
 
 int main() {
