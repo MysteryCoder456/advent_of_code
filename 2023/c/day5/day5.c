40fdd0aa0849c94fa2580c9c342896016debbb4d
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-05-05 22:48:03 +0400

    2023 C day 5 part 2 (a lot faster)

--- 2023/c/day5/day5.c
+++ 2023/c/day5/day5.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -91,6 +92,50 @@ uint64_t part1() {
     return arrayMin(seeds, seedCount);
 }
 
+// Part 2 stuff below
+
+uint64_t ranges[7][64][3];
+int rangeCounts[7];
+
+typedef struct {
+    uint64_t start;
+    uint64_t size;
+} inner_arg;
+
+void *innerThread(void *thread_arg) {
+    inner_arg *arg = thread_arg;
+    uint64_t *minLocation = malloc(sizeof(uint64_t));
+    *minLocation = UINT64_MAX;
+
+    for (uint64_t seed = arg->start; seed < arg->start + arg->size; seed++) {
+        uint64_t mappedSeed = seed;
+
+        // if (seed % 10000000 == 0)
+        //     printf("processing seed %lld\n", seed);
+
+        // Sections
+        for (int i = 0; i < 7; i++) {
+            int rangeCount = rangeCounts[i];
+
+            // Ranges
+            for (int j = 0; j < rangeCount; j++) {
+                uint64_t *range = ranges[i][j];
+                uint64_t seedBefore = mappedSeed;
+                mappedSeed = mapRange(mappedSeed, range[0], range[1], range[2]);
+
+                if (mappedSeed != seedBefore)
+                    break;
+            }
+        }
+
+        if (mappedSeed < *minLocation)
+            *minLocation = mappedSeed;
+    }
+
+    free(arg);
+    return minLocation;
+}
+
 uint64_t part2() {
     FILE *fptr = fopen("day5.txt", "r");
     char lineBuffer[MAX_LINE];
@@ -99,8 +144,6 @@ uint64_t part2() {
     fgets(lineBuffer, MAX_LINE, fptr);
     fgets(lineBuffer, MAX_LINE, fptr);
 
-    uint64_t ranges[7][64][3];
-    int rangeCounts[7];
     int section = 0;
 
     // Parse map sections
@@ -135,44 +178,38 @@ uint64_t part2() {
     char *seedsBuffer = &lineBuffer[7];
 
     // Parse seed ranges and find minimum location value
-    // FIXME: very very slow, pls multithread
-    uint64_t minLocation = UINT64_MAX;
+    pthread_t handles[16];
+    size_t handle_count = 0;
     char *first;
     while ((first = strsep(&seedsBuffer, " ")) != NULL) {
         char *second = strsep(&seedsBuffer, " ");
 
         uint64_t start = atol(first);
         uint64_t size = atol(second);
-        printf("processing seed range %lld : %lld\n", start, size);
+        // printf("processing seed range %lld : %lld\n", start, size);
 
-        // Seeds
-        for (uint64_t seed = start; seed < start + size; seed++) {
-            uint64_t mappedSeed = seed;
+        // Thread arguments
+        inner_arg *arg = malloc(sizeof(inner_arg));
+        arg->start = start;
+        arg->size = size;
 
-            if (seed % 10000000 == 0)
-                printf("processing seed %lld\n", seed);
-
-            // Sections
-            for (int i = 0; i < 7; i++) {
-                int rangeCount = rangeCounts[i];
-
-                // Ranges
-                for (int j = 0; j < rangeCount; j++) {
-                    uint64_t *range = ranges[i][j];
-                    uint64_t seedBefore = mappedSeed;
-                    mappedSeed =
-                        mapRange(mappedSeed, range[0], range[1], range[2]);
-
-                    if (mappedSeed != seedBefore)
-                        break;
-                }
-            }
-
-            if (mappedSeed < minLocation)
-                minLocation = mappedSeed;
-        }
+        // Create thread to process seeds
+        pthread_t t;
+        pthread_create(&t, NULL, innerThread, arg);
+        handles[handle_count] = t;
+        handle_count++;
     }
 
+    uint64_t minLocation = UINT64_MAX;
+    for (int i = 0; i < handle_count; i++) {
+        int *location;
+        pthread_join(handles[i], (void **)&location);
+
+        if (*location < minLocation)
+            minLocation = *location;
+
+        free(location);
+    }
     return minLocation;
 }
 
